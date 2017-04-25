e73e5bf735e5a5997de4f225e82c596ada6ab463
bogdan <bodia005@gmail.com>
2017-04-25 14:57:20 +0300

    lab2.4 finish program

--- Lab2.4MPI/Lab2.4MPI/Lab2.4MPI/main.cpp
+++ Lab2.4MPI/Lab2.4MPI/Lab2.4MPI/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include <limits.h>
 #include "mpi.h"
 
 using namespace std;
 
-const int N = 6;
-const int P = 6;
-const int H = N / P;
+const int N = 600;
 
 void inputMatrix(int matrix[N][N])
 {
@@ -46,6 +45,17 @@ void outputVector(int vector[N])
 	cout << endl;
 }
 
+int maxElement(int vector[N], int from, int to)
+{
+	int result = INT_MIN;
+	for (int i = 0; i < N; i++)
+	{
+		if (vector[i] > result)
+			result = vector[i];
+	}
+	return result;
+}
+
 
 
 int main(int argc, char* argv[])
@@ -53,16 +63,18 @@ int main(int argc, char* argv[])
 
 	MPI_Init(&argc, &argv);
 
-	int rank;
+	int rank, size;
 
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int P = size;
+	int H = N / P;
 
 	int A[N], B[N], E[N], Z[N];
 	int MO[N][N], MK[N][N];
 	int d, a;
 	MPI_Status status;
-
-	cout << "Thread N_" << rank << endl;
 
 	if (rank == 0)
 	{
@@ -190,23 +202,79 @@ int main(int argc, char* argv[])
 		MPI_Send(MO, N*N, MPI_INT, 4, 0, MPI_COMM_WORLD);
 	}
 
-	/*if (rank == 4)
+	int startIndex = rank*H;
+	int endIndex = rank == P - 1 ? N : (rank + 1) * H;
+
+	int a_i = maxElement(Z, startIndex, endIndex);
+
+	MPI_Allreduce(&a_i, &a, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+	for (int i = startIndex; i < endIndex; i++)
 	{
-		cout << "Thread N_" << rank << endl;
-		cout << "Int d: " << d << endl;
-		cout << "Vector Z:" << endl;
-		outputVector(Z);
-		cout << "Vector E:" << endl;
-		outputVector(E);
-		cout << "Vector B:" << endl;
-		outputVector(B);
-		cout << "Matrix MO:" << endl;
-		outputMatrix(MO);
-		cout << "Matrix MK:" << endl;
-		outputMatrix(MK);
-	}*/
+		int tempV[N];
+		for (int j = 0; j < N; j++)
+		{
+			int M = 0;
+			for (int k = 0; k < N; k++)
+			{
+				M += MK[i][k] * MO[k][j];
+			}
+			tempV[j] = M;
+		}
+
+		int resultNumber = 0;
+		for (int j = 0; j < N; j++)
+		{
+			resultNumber += tempV[i] * B[i];
+		}
+
+		A[i] = a * E[i] + d * resultNumber;
+	}
 
 
+	if (rank == 5)
+	{
+		MPI_Send(A + (H * 5), N - (H * 5), MPI_INT, 4, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 4)
+	{
+		MPI_Recv(A + (H * 5), N - (H * 5), MPI_INT, 5, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Send(A + (H * 4), N - (H * 4), MPI_INT, 3, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 3)
+	{
+		MPI_Recv(A + (H * 4), N - (H * 4), MPI_INT, 4, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Send(A + (H * 3), N - (H * 3), MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 2)
+	{
+		MPI_Send(A + (H * 2), H, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 1)
+	{
+		MPI_Recv(A + (H * 2), H, MPI_INT, 2, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Send(A + H, 2 * H, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 0)
+	{
+		MPI_Recv(A + H, 2 * H, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Recv(A + (H * 3), N - (H * 3), MPI_INT, 3, 0, MPI_COMM_WORLD, &status);
+	}
+
+
+	//output
+	if (rank == 0)
+	{
+		cout << "Result A:" << endl;
+		if (N <= 15)
+			outputVector(A);
+		else
+			cout << "Vector is too large." << endl;
+	}
 
 	MPI_Finalize();
 	return 0;
